d9ad24493335ea4552fbca8fb3dde587d8b8746e
marialetiziavecchi <81481547+marialetiziavecchi@users.noreply.github.com>
2021-03-29 15:23:25 +0200

    Update index.php

--- quotes/index.php
+++ quotes/index.php
@@ -1,3 +1,5 @@
 <?php
     print "This app returns a random funny quote.\n"
+        <?php    $link = mysqli_connect($_ENV["DATABASE_SERVICE_NAME"],$_ENV["DATABASE_USER"],$_ENV["DATABASE_PASSWORD"],$_ENV["DATABASE_NAME"]);    if (!$link) {        http_response_code (500);        error_log ("Error: unable to connect to database\n");        die();    }
+        
 ?>
